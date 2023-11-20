17ff8f9fcdbda3e46d47b7d2356719d7a9bccf1e
YuryRegis <yury_regis@hotmail.com>
2023-11-20 15:17:58 -0300

    chore: create design provider

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {ThemeProvider} from '@shopify/restyle';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {theme} from '@theme';
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,7 @@ export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <ThemeProvider theme={{}}>
+        <ThemeProvider theme={theme}>
           <></>
         </ThemeProvider>
       </SafeAreaProvider>
