09020e1ae1d50f97cf58ebc30cfcfa8e21a9082e
YuryRegis <yury_regis@hotmail.com>
2023-11-21 08:40:24 -0300

    chore: add Icon component

---
+++ src/components/Icon/IconProps.ts
@@ -0,0 +1,85 @@
+import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    BellIcon,
+    BellOnIcon,
+    BookmarkFillIcon,
+    BookmarkIcon,
+    CameraIcon,
+    ChatIcon,
+    ChatOnIcon,
+    CheckIcon,
+    CheckRoundIcon,
+    ChevronRightIcon,
+    CommentIcon,
+    ErrorRoundIcon,
+    EyeOffIcon,
+    EyeOnIcon,
+    FlashOffIcon,
+    FlashOnIcon,
+    HeartFillIcon,
+    HeartIcon,
+    HomeFillIcon,
+    HomeIcon,
+    MessageIcon,
+    MessageRoundIcon,
+    MessageRoundLightIcon,
+    NewPostIcon,
+    ProfileFillIcon,
+    ProfileIcon,
+    SearchIcon,
+    SettingsIcon,
+    TrashIcon,
+} from '@icons';
+import {ThemeColors} from '@theme';
+  
+export interface IconProps {
+    name: IconName;
+    color?: ThemeColors;
+    size?: number;
+    onPress?: () => void;
+}
+  
+export interface IconBase {
+    size?: number;
+    color?: string;
+}
+  
+export const iconRegistry = {
+    arrowLeft: ArrowLeftIcon,
+    arrowRight: ArrowRightIcon,
+    bell: BellIcon,
+    bellOn: BellOnIcon,
+    bookmarkFill: BookmarkFillIcon,
+    bookmark: BookmarkIcon,
+    camera: CameraIcon,
+    chat: ChatIcon,
+    chatOn: ChatOnIcon,
+    check: CheckIcon,
+    checkRound: CheckRoundIcon,
+    chevronRight: ChevronRightIcon,
+    comment: CommentIcon,
+    errorRound: ErrorRoundIcon,
+    eyeOn: EyeOnIcon,
+    eyeOff: EyeOffIcon,
+    flashOff: FlashOffIcon,
+    flashOn: FlashOnIcon,
+    heartFill: HeartFillIcon,
+    heart: HeartIcon,
+    homeFill: HomeFillIcon,
+    home: HomeIcon,
+    message: MessageIcon,
+    messageRound: MessageRoundIcon,
+    messageRoundLight: MessageRoundLightIcon,
+    newPost: NewPostIcon,
+    profileFill: ProfileFillIcon,
+    profile: ProfileIcon,
+    search: SearchIcon,
+    settings: SettingsIcon,
+    trash: TrashIcon,
+  };
+  
+type IconType = typeof iconRegistry;
+  
+export type IconName = keyof IconType;
+  
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export {Text} from './Text';
+export { Icon } from './Icon';
 export {Button} from './Button';
 export {Box, TouchableOpacityBox} from './Box';
 export {ActivityIndicator} from './ActivityIndicator';
 
 export type {BoxProps, TouchableOpacityBoxProps} from './Box';
+export type {IconBase, IconProps, IconName} from './Icon/IconProps';
