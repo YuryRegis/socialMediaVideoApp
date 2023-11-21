7907c1b7dbc0fdbff222a904d56f31dce01b1f33
YuryRegis <yury_regis@hotmail.com>
2023-11-21 08:14:27 -0300

    chore: add Button + ActivityIndicator + useTheme

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {ActivityIndicator, Text, TouchableOpacityBox} from '@components';
+import {buttonPresets} from './ButtonPresets';
+import {ButtonProps} from './ButtonProps';
+
+export function Button({
+  title,
+  loading,
+  preset = 'primary',
+  disabled,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
+
+  return (
+    <TouchableOpacityBox
+        testID="button"
+        disabled={disabled || loading}
+        paddingHorizontal="s20"
+        height={50}
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="s16"
+        {...buttonPreset.container}
+        {...touchableOpacityBoxProps}>
+        {loading ? (
+            <ActivityIndicator
+            testID="activity-indicator"
+            color={buttonPreset.content}
+            />
+        ) : (
+            <Text preset="paragraphMedium" bold color={buttonPreset.content}>
+            {title}
+            </Text>
+        )}
+    </TouchableOpacityBox>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
 export {Text} from './Text';
+export {Button} from './Button';
 export {Box, TouchableOpacityBox} from './Box';
+export {ActivityIndicator} from './ActivityIndicator';
+
+export type {BoxProps, TouchableOpacityBoxProps} from './Box';
