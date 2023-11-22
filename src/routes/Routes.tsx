7d3df450d1f8c605436e23eee8c49405c38aaa0a
YuryRegis <yury_regis@hotmail.com>
2023-11-22 11:08:20 -0300

    fix: app freezing on start in Android devices

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
 import {useAuthCredentials} from '@services';
+import {Screen, ActivityIndicator} from '@components';
 
-
+//TODO: implement fix to authCredentials on Android devices
 export function Router() {
   const {authCredentials, loading} = useAuthCredentials();
 
-  if (loading) {
-    // TODO: Add a loading screen
-    return null;
+  function LoadingScreen() {
+    return (
+      <Screen flex={1} justifyContent='center' alignItems='center'>
+          <ActivityIndicator size='large' color='backgroundContrast'/>
+      </Screen>
+    );
   };
 
   return (
     <NavigationContainer>
+      {/* {loading ? <LoadingScreen/> : (
+        Boolean(authCredentials) ? <AppStack /> : <AuthStack />
+      )} */}
       {Boolean(authCredentials) ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
