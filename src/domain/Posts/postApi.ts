ba5b21ffa2fd03a76775e3b579e0c8776337fb42
YuryRegis <yury_regis@hotmail.com>
2023-11-25 17:39:38 -0300

    chore: create getListByUserID service

--- src/domain/Posts/postApi.ts
+++ src/domain/Posts/postApi.ts
@@ -1,11 +1,17 @@
 import {Post} from './postTypes';
-import {postListMock} from './postListMock';
+import {postListMock, postListByIdMock} from './postListMock';
 
 async function getList(): Promise<Post[]> {
   await new Promise(resolve => setTimeout(() => resolve(''), 1500));
   return postListMock;
 }
 
+async function getListByUserID(userID: string): Promise<Post[]> {
+  await new Promise(resolve => setTimeout(() => resolve(`${userID}`), 1500));
+  return postListByIdMock;
+}
+
 export const postApi = {
-  getList,
+  getListByUserID,
+  getList
 };
--- src/domain/Posts/postService.ts
+++ src/domain/Posts/postService.ts
@@ -1,11 +1,18 @@
 import {postApi} from './postApi';
 import {Post} from './postTypes';
 
+
 async function getList(): Promise<Post[]> {
   const postList = await postApi.getList();
   return postList;
 }
 
+async function getListByUserID(userID: string): Promise<Post[]> {
+  const postListByUserID = await postApi.getListByUserID(userID);
+  return postListByUserID;
+}
+
 export const postService = {
   getList,
+  getListByUserID
 };
