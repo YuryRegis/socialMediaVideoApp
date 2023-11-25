9cf7b04744bef1f7cde6f7666ba2be4b8bcfb4bd
YuryRegis <yury_regis@hotmail.com>
2023-11-25 17:40:33 -0300

    chore: create mocks

---
+++ src/domain/User/userMock.ts
@@ -0,0 +1,17 @@
+import {User} from "./userTypes";
+
+export const userMock: User = {
+    id: '1',
+    isOnline: true,
+    email: 'user@mock.com',
+
+    firstName: 'João',
+    username: 'icedmock',
+    lastName: 'das Neves',
+    fullName: 'João das Neves',
+    profileURL: 'https://cg4.cgsociety.org/uploads/images/medium/hosseindiba-jon-snow-1-b968195d-au6q.jpg',
+    
+    folowingCount: 794,
+    folowersCount: 1238,
+    publishedPostsCount: 11,
+};  
